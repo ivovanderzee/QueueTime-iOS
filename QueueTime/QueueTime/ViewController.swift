c4f9f8e16cf78553cbcd7850ed2fff80fdcf0d3a
ivovanderzee <ivovanderzee@live.nl>
2018-11-13 11:17:21 +0100

    Project aangemaakt en firebase geinstalleerd. Podfiles zijn toegevoegd. Vanaf nu altijd werken in QueueTime.xcworkspace

---
+++ QueueTime/QueueTime/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  QueueTime
+//
+//  Created by Ivo van der Zee on 06/11/2018.
+//  Copyright © 2018 Ivo van der Zee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Outlets voor het loginscherm
+    @IBOutlet weak var userNameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    //Button die ervoor een actie aanroept als de gebruiker wilt inloggen
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+    }
+
+
+}
+
