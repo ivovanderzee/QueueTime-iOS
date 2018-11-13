b68c9880a87bdfeebd6acec37be1185bd8923dae
Tim <timvdzee@live.nl>
2018-11-13 12:03:43 +0100

    Kaarten ViewController toegevoegd aan project.

---
+++ QueueTime/QueueTime/locationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  locationViewController.swift
+//  QueueTime
+//
+//  Created by Tim van der Zee on 13/11/2018.
+//  Copyright © 2018 Ivo van der Zee. All rights reserved.
+//
+
+import Foundation
---
+++ QueueTime/locationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  locationViewController.swift
+//  
+//
+//  Created by Tim van der Zee on 13/11/2018.
+//
+
+import UIKit
+
+class locationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
