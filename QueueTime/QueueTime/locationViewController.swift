189c29e4d1ade4a14948f965ccd52ecd30d14071
Tim <timvdzee@live.nl>
2018-11-13 12:08:30 +0100

    LocationViewController toegevoegd aan project.

--- QueueTime/QueueTime/locationViewController.swift
+++ QueueTime/QueueTime/locationViewController.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2018 Ivo van der Zee. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class locationViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // Outlet voor de kaart
+    
+    
+    // Location manager
+    var locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        // If statement voor als locatie is aangezet en geaccepteerd
+        if CLLocationManager.locationServicesEnabled()
+        {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+    }
+    
+    // Functie die wordt aangeroepen om elke keer de locatie op te vragen
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        let userLocation = locations.last
+        let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
+        self.map.setRegion(viewRegion, animated: true)
+        
+        self.map.showsUserLocation = true
+    }
+    
+    
+}
+
+
--- QueueTime/locationViewController.swift
+++ QueueTime/locationViewController.swift
@@ -6,24 +6,41 @@
 //
 
 import UIKit
+import MapKit
 
-class locationViewController: UIViewController {
+class locationViewController: UIViewController, CLLocationManagerDelegate {
 
+    // Outlet voor de kaart
+    @IBOutlet weak var map: MKMapView!
+    
+    // Location manager
+    var locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        // If statement voor als locatie is aangezet en geaccepteerd
+        if CLLocationManager.locationServicesEnabled()
+        {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Functie die wordt aangeroepen om elke keer de locatie op te vragen
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        let userLocation = locations.last
+        let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
+        self.map.setRegion(viewRegion, animated: true)
+        
+        self.map.showsUserLocation = true
     }
-    */
+
 
 }
