ea4277f6b7094b9257123f0e72fc32a5202db191
Xandor Naus <Xandor@MacBook-Pro-van-Xandor.local>
2018-11-19 12:59:48 +0100

    Add functie aangepast, kan nu Data in Firebase zetten.

--- QueueTime/QueueTime/profileViewController.swift
+++ QueueTime/QueueTime/profileViewController.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2018 Ivo van der Zee. All rights reserved.
 //
 
-/*
+
 import UIKit
 
 import Firebase
+
+let ref = Database.database().reference(withPath: "Personalinfo")
 
 class profileViewController: UIViewController {
     
@@ -21,56 +23,24 @@ class profileViewController: UIViewController {
     //Actie die word uitgevoerd als er op de login button word gedrukt
     @IBAction func sendinfoButton(_ sender: Any) {
         
+        //Actie die word uitgevoerd als er op de login button word gedrukt
+        let PeronsalinfoDB = Database.database().reference().child("Personalinfo")
         
-        var refArtists: DatabaseReference!
-        
-        
-        
-        func viewDidLoad() {
-            super.viewDidLoad()
-            // Voer een extra installatie uit nadat de weergave is geladen, meestal vanaf een punt.
+        // Values die in de twee textvelden staan worden toegevoegd in Firebase-database.
+        let PeronsalinfoDictionary : NSDictionary = ["age" : malefemaleTextField.text!, "gender" : ageTextField.text!]
+        PeronsalinfoDB.childByAutoId().setValue(PeronsalinfoDictionary) {
+            (error, ref) in
             
-            FirebaseApp.configure()
-            
-            // Een verwijzing naar de knooppuntartiesten krijgen
-            refArtists = Database.database().reference().child("artists");
-        
-        func addInfo(){
-            
-            let key = refArtists.childByAutoId().key
-            
-            // Info aangemaakt met bijbehoorde values.
-            let artist = ["id":key,
-                          "age": ageTextField.text! as String,
-                          "gender": malefemaleTextField.text! as String
-            ]
-            
-            // De info een unieke key meegeven, voor desbetreffende user.
-            refArtists.child(key).setValue(artist)
-            
-    
-        
+            // Actie is niet uitgevoerd, en geeft melding.
+            if error != nil {
+                print(error!)
+                
+                // Actie is succesfully uitgevoerd, en geeft melding.
+            }
+            else {
+                print("Message saved successfully!")
+            }
         }
-
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-        }
-        
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
-        
-        
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //configuring firebase
-        FirebaseApp.configure()
         
     }
     
@@ -82,13 +52,13 @@ class profileViewController: UIViewController {
  // MARK: - Navigation
  
  // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
  // Get the new view controller using segue.destination.
  // Pass the selected object to the new view controller.
- }
+
  
 
-}
 
 
-*/
+
+
